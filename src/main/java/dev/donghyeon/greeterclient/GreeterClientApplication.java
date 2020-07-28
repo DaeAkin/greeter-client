6b906f5abf24198c7337eadfa791403dbdcee93e
Donghyeon Min <mindonghyeon890@gmail.com>
2020-07-28 15:00:17 +0900

    first commit

---
+++ src/main/java/dev/donghyeon/greeterclient/GreeterClientApplication.java
@@ -0,0 +1,25 @@
+package dev.donghyeon.greeterclient;
+
+import dev.donghyeon.greeter.Greeter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeterClientApplication implements CommandLineRunner {
+
+	@Autowired
+	private Greeter greeter;
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreeterClientApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+        String message = greeter.greet();
+        System.out.println(message);
+	}
+
+}
---
+++ src/test/java/dev/donghyeon/greeterclient/GreeterClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.donghyeon.greeterclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeterClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
